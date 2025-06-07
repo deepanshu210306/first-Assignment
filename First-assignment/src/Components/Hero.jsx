0032cb8c00af832e1d02110af255a0a96d20d959
deepanshu210306 <deepanshu210306@gmail.com>
2025-06-07 22:54:05 +0530

    Updated Hero section and added header clip video

--- First-assignment/src/Components/Hero.jsx
+++ First-assignment/src/Components/Hero.jsx
@@ -1,8 +1,16 @@
 import "./Hero.css";
-
+import headerClip from "./header_clip.mp4";
 const Hero = () => {
   return (
     <div className="hero">
+        <video
+        autoPlay
+        muted
+        loop
+        className="hero-video">
+        <source src={headerClip} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       <div className="hero-content">
         <h1>Your Dream Study Abroad Journey Starts Here</h1>
         <h2>Expert IELTS Training & Visa Guidance for Top Universities in USA, UK, Canada, Europe, Germany & Australia</h2>
